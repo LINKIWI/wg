6b645b751e70e640e45e1bc4616a3c9644d327e3
Kevin Lin <developer@kevinlin.info>
2019-08-08 19:55:55 -0700

    internal/cli: ArrayFlag

--- internal/cli/flags.go
+++ internal/cli/flags.go
@@ -14,9 +14,21 @@ type ChoicesFlag struct {
 	choice        string
 }
 
+// ArrayFlag describes a flag.Value with multiple possible values.
+type ArrayFlag struct {
+	flag.Value
+
+	values []string
+}
+
 // NewChoicesFlag creates a new ChoicesFlag with candidate choices and a default choice.
 func NewChoicesFlag(choices []string, defaultChoice string) *ChoicesFlag {
 	return &ChoicesFlag{candidates: choices, defaultChoice: defaultChoice}
+}
+
+// NewArrayFlag creates a new ArrayFlag.
+func NewArrayFlag() *ArrayFlag {
+	return &ArrayFlag{values: make([]string, 0)}
 }
 
 // Set is called when the associated flag is specified on the CLI.
@@ -43,4 +55,21 @@ func (cf *ChoicesFlag) Choice() string {
 // String acts as an alias for Choice.
 func (cf *ChoicesFlag) String() string {
 	return cf.Choice()
+}
+
+// Set appends the value to the array in internal state.
+func (af *ArrayFlag) Set(value string) error {
+	af.values = append(af.values, value)
+
+	return nil
+}
+
+// Values returns the values set for the flag.
+func (af *ArrayFlag) Values() []string {
+	return af.values
+}
+
+// String provides a string representation of the string array.
+func (af *ArrayFlag) String() string {
+	return fmt.Sprintf("%v", af.values)
 }
